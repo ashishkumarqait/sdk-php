741c938719048e2947468a9006345fa589dbfb48
tam5 <arimiller92@gmail.com>
2021-10-18 10:43:22 -0400

    chore: bump linter

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = Symfony\Component\Finder\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new LiveIntent\PhpCsFixer\Config())->setFinder($finder);
